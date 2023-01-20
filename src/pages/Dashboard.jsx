e848ebf6c7e9922a4ba88c798380e1111b1200fd
franklin <igahfranklyn@gmail.com>
2023-01-20 14:11:46 +0100

    grid adjusted

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='grid grid-cols-3'>
+    <div className='grid grid-cols-2'>
       <div className='bg-red-500'>hello</div>
       <div className='bg-red-900'>2</div>
       <div className='bg-red-200'>3</div>
